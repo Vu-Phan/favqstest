c9d5a53c388bc6a6bfa3c77ae86014ca941c886a
Vu Phan <vu.phan.fr@gmail.com>
2021-09-12 11:39:02 +0200

    STEP 1 : Setup project - target deployment / folder arch / cocoapods

---
+++ favqstest/favqstest/Interface/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  favqstest
+//
+//  Created by Vu Phan on 12/09/2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
