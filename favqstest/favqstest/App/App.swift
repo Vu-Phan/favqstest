2ab29aada188d78be4655240add71d7b84ac4191
Vu Phan <vu.phan.fr@gmail.com>
2021-09-12 12:49:56 +0200

    STEP 4 : First Screen / Auth session in LoginVC

--- favqstest/favqstest/App/App.swift
+++ favqstest/favqstest/App/App.swift
@@ -12,7 +12,25 @@ class App {
     static let shared = App()
     private init () {}
     
-
+    var user: UserM? = nil
+    
+    func isLoginSessionExist() -> String? {
+        if let _ = App.pref.get(forKey: .token) as? String,
+           let login = App.pref.get(forKey: .login) as? String {
+            return login
+        }
+        return nil
+    }
+    
+    func saveSession(_ session: SessionM) {
+        App.pref.save(forKey: .token, value: session.token)
+        App.pref.save(forKey: .login, value: session.login)
+    }
+    
+    func deleteSession() {
+        App.pref.save(forKey: .token, value: nil)
+        App.pref.save(forKey: .login, value: nil)
+    }
 }
 
 extension App {
---
+++ favqstest/favqstest/Interface/LoginVC.swift
@@ -0,0 +1,90 @@
+//
+//  ViewController.swift
+//  favqstest
+//
+//  Created by Vu Phan on 12/09/2021.
+//
+
+import UIKit
+
+class LoginVC: UIViewController {
+    // MARK: Xib
+    @IBOutlet weak var formContentView: UIView!
+    @IBOutlet weak var loginLabel: UILabel!
+    @IBOutlet weak var loginTextField: UITextField!
+    @IBOutlet weak var passwordLabel: UILabel!
+    @IBOutlet weak var passwordTextField: UITextField!
+    @IBOutlet weak var confirmButton: AppButton!
+    
+    
+    // MARK: - View Controller
+    override func loadView() {
+        super.loadView()
+        setupUI()
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        // TEST PURPOSE
+        loginTextField.text = "piodecitron@gmail.com"
+        passwordTextField.text = "97D6ZjsPPke"
+        
+        checkUserToken()
+    }
+    
+    
+    // MARK: - UI
+    private func setupUI() {
+        formContentView.pr_roundBackground(color: UIColor.app.main02, radius: 8)
+        
+        loginLabel.pr_setup(font: UIFont.app.body1, color: UIColor.app.text02)
+        passwordLabel.pr_setup(font: UIFont.app.body1, color: UIColor.app.text02)
+        passwordTextField.isSecureTextEntry = true
+        
+        loginLabel.text = "Login"
+        passwordLabel.text = "Password"
+        confirmButton.style1(text: "Continuer")
+    }
+    
+    
+    // MARK: - Data
+    private func checkUserToken() {
+        if let login = App.shared.isLoginSessionExist() {
+            ServiceFavqs.shared.getUser(login: login, done: { user in
+                if let user = user {
+                    dump(user)
+                    App.shared.user = user
+                    self.showNext()
+                } else {
+                    App.shared.deleteSession()
+                }
+            })
+        }
+    }
+    
+    
+    // MARK: - Navigation
+    private func showNext() {
+
+    }
+    
+    
+    // MARK: - Xib handlers
+    @IBAction func confirmButtonHandler(_ sender: Any) {
+        confirmButton.isEnabled = false
+        
+        if let login = loginTextField.text, let pwd = passwordTextField.text, !login.isEmpty && !pwd.isEmpty {
+            ServiceFavqs.shared.getSession(login: login, pwd: pwd, done: { session in
+                if let session = session {
+                    App.shared.saveSession(session)
+                    self.checkUserToken()
+                }
+                
+                self.confirmButton.isEnabled = true
+            })
+        }
+        
+    }
+}
+
